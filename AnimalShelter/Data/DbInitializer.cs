63b6df621739dc87907af620298226a0b6818738
Ahmed Hasanov <ahmedhasanovahmedcho@gmail.com>
2026-01-19 14:10:30 +0200

    Added class DbInitializer in Data folder

---
+++ AnimalShelter/Data/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+using AnimalShelter.Models.Enums;
+
+namespace AnimalShelter.Data
+{
+    public static class DbInitializer
+    {
+    }
+}
