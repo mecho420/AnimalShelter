27300ccb23e4d1244df6f82b29510f0ab5b77bc4
Ahmed Hasanov <ahmedhasanovahmedcho@gmail.com>
2026-01-25 19:39:42 +0200

    Added a new migration 20260125172835_AddUserNavigationToAdoptionRequest

---
+++ AnimalShelter/Migrations/20260125172835_AddUserNavigationToAdoptionRequest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNavigationToAdoptionRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AdoptionRequests",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdoptionRequests_UserId",
+                table: "AdoptionRequests",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AdoptionRequests_AspNetUsers_UserId",
+                table: "AdoptionRequests",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AdoptionRequests_AspNetUsers_UserId",
+                table: "AdoptionRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AdoptionRequests_UserId",
+                table: "AdoptionRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AdoptionRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
